6c84f6a7f3c75ec83c6dfcf8fc412498b6121920
Iftekhar Bin Aziz <iftekhar932@gmail.com>
2023-03-20 22:29:59 +0600

    json data fetch done

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
+// JSON DATA
+const courseData = require("./data/data.json");
+
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("hello");
+});
+
+app.get("/courses", (req, res) => {
+  res.send(courseData);
 });
 
 app.listen(port, () => {
