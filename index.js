d8424d867a4e4a4ce35d62658bff02576127813e
Iftekhar Bin Aziz <iftekharaziz932@gmail.com>
2023-04-06 00:08:59 +0600

    query

--- index.js
+++ index.js
@@ -15,6 +15,18 @@ app.get("/", (req, res) => {
 app.get("/courses", (req, res) => {
   res.send(courseData);
 });
+// this one is working but i want to learn how to do it with query instead of params
+/* app.get("/courses/:category", (req, res) => {
+  const categoryName = req.params.category;
+  const filteredCourses = courseData.filter((c) => c.category == categoryName);
+  res.send(filteredCourses);
+});
+ */
+app.get("/courses/:category", (req, res) => {
+  const categoryName = req.query.category;
+  const filteredCourses = courseData.filter((c) => c.category == categoryName);
+  res.send(filteredCourses);
+});
 
 app.listen(port, () => {
   console.log("listening on port " + port);
